ee25b001ff7879fec87e102c5d1b02d380253dce
raghav <nitro@raghavs-MacBook-Pro.local>
2023-03-06 16:56:32 +0530

    added clojure_by_example and solved problem number 47

--- solutions_4clojure/easy.clj
+++ solutions_4clojure/easy.clj
@@ -1,2 +1,9 @@
 (ns solutions_4clojure.easy)
 
+;; Problem 47, Contain Yourself 
+;; (contains? #{4 5 6} __)
+;; (contains? [1 1 1 1 1] __)
+;; (contains? {4 :a 2 :b} __)
+;; (not (contains? [1 2 4] __))
+4
+
