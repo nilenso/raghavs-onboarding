269b3f29c844d145ccc3cdd3d7ffb9b8b5669e4e
raghavyuva <vikramnbhat15@gmail.com>
2023-03-08 17:07:10 +0530

    solutions 25-29

--- solutions_4clojure/easy.clj
+++ solutions_4clojure/easy.clj
@@ -1,4 +1,5 @@
 (ns solutions_4clojure.easy)
+(require '[clojure.string :as str])
 
 ;; Problem 19, Last Element 
 ;; (= (__ [1 2 3 4 5]) 5)
@@ -40,10 +41,10 @@
       (recur (rest x) (inc i)))))
 
 (defn countseq-recur [items]
-  (reduce (fn [i,items] (inc i)) 0 items))
+  (reduce (fn [cnt item] (inc cnt)) 0 items))
 
 (countseq [1 2 3 4 5 6 5 6 3])
-(countseq-recur [1 2 3 4 5 6 5 6 3,6,7])
+(countseq-recur [1 2 3 4 5 6 5 6 3 6 7])
 
 ;; Problem 23, Reverse a Sequence
 ;; (= (__ [1 2 3 4 5]) [5 4 3 2 1])
@@ -64,27 +65,83 @@
 ;; (= (__ '(0 0 -1)) -1)
 ;; (= (__ '(1 10 3)) 14)
 (defn sumup [items]
-  (loop [x items i (count items) sum 0]
-    (if (<= i 0)
+  (loop [x items sum 0]
+    (if (empty? x)
       sum
-      (recur (rest x) (dec i) (+ sum (first x))))))
+      (recur (rest x) (+ sum (first x))))))
 (sumup [1 2 5 6 8])
 
-;; still working on this problem
+(defn sum-up [items]
+  (reduce + 0 items))
+
+(sum-up [1 2 3 4])
+
 ;; Problem 25, Find the odd numbers 
 ;; (= (__ #{1 2 3 4 5}) '(1 3 5))
 ;; (= (__ [4 2 1 6]) '(1))
 ;; (= (__ [2 2 4 6]) '())
 ;; (= (__ [1 1 1 3]) '(1 1 1 3))
 (defn find-odd [items]
-  (loop [x items i (count items) newitems []]
-    (if (<= i 0)
+  (loop [x items newitems []]
+    (if (empty? x)
       newitems
-      (recur(rest x)(dec i)(conj newitems odd? (first x)))
-      )
-  )
-   )
-(find-odd [1 5 7 8 6 4])
+      (if (odd? (first x))
+        (recur (rest x) (conj newitems (first x)))
+        (recur (rest x) newitems)))))
+(find-odd [1 5 2 7])
+
+;; should try printing a sequence 
+;; Problem 26, Fibonacci Sequence
+;; (= (__ 3) '(1 1 2))
+;; (= (__ 6) '(1 1 2 3 5 8))
+;; (= (__ 8) '(1 1 2 3 5 8 13 21))
+
+
+
+;; Problem 27, Palindrome Detector 
+;; (false? (__ '(1 2 3 4 5)))
+;; (true? (__ "racecar"))
+;; (true? (__ [:foo :bar :foo]))
+;; (true? (__ '(1 1 3 3 1 1)))
+;; (false? (__ '(:a :b :c)))
+(defn palindrome [st]
+  (= (seq st) (reverse st)))
+(palindrome "racecar")
+
+;; still working on this problem
+;; Problem 28, Flatten a Sequence 
+;; (= (__ '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+;; (= (__ ["a" ["b"] "c"]) '("a" "b" "c"))
+;; (= (__ '((((:a))))) '(:a))
+(defn flatten-list [lst]
+  (if (not (empty? lst))
+    (if (sequential? lst)
+      (loop [items lst
+             outlst []]
+        (if (empty? items)
+          outlst
+           (if (sequential? (first items)) 
+          (recur (first items) 
+                 (conj outlst (first items)) 
+                 ))))))
+  
+  (flatten-list [1 [4 5] 2 3]))
+
+;; Problem 29, Get the Caps 
+;; (= (__ "HeLlO, WoRlD!") "HLOWRD")
+;; (empty? (__ "nothing"))
+;; (= (__ "$#A(*&987Zf") "AZ")
+(defn get-caps [st]
+  (loop [items st newstr ""] 
+      (if (empty? items)
+        newstr
+        (if (= (first items) (str/upper-case (first items)))
+          (recur (rest items) (conj newstr (first items)))
+          (recur (rest items) newstr)
+        )
+    )
+  )) 
+(get-caps "HeLlO, WoRlD!")
 
 ;; Problem 47, Contain Yourself 
 ;; (contains? #{4 5 6} __)
